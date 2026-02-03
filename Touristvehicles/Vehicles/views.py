91205f9e5656c7a19be750c93a8373c66557ed46
unknown <anjalybiju049@gmail.com>
2026-02-03 12:42:01 +0530

    first commit

---
+++ Touristvehicles/Vehicles/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'Base.html')
+
+def home(request):
+    return render(request,'Index.html')
+
+def about(request):
+    return render(request,'About.html')
+
+def contact(request):
+    return render(request,'Contact.html')
+
+def packages(request):
+    return render(request,'Packages.html')
+
+def services(request):
+    return render(request,'Services.html')
